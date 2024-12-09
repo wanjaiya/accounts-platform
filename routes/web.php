1c7d5a8320c47965f272559157764f689285918b
wanjaiya <kangethewanjaiya@gmail.com>
2024-12-09 09:23:24 +0300

    setup

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes([
+    'verify'=> 'true',
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
